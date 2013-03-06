049902f394381d4a34de62d99fe30489653ba12c
Serena23 <serenasforza@tiscali.it>
2013-03-06 18:33:23 +0100

    exercise03

---
+++ exercise03.js
@@ -0,0 +1,16 @@
+var j;
+var i;
+for (j = 1; j <= 10; j +=1) {
+	for (i = 1; i <= 10; i +=1) {
+  		if(i!==j){
+  			if (i!==10) {
+  				console.log(0+","+\t");
+  			else
+  				console.log(0+\t");
+  			}
+  		}
+  		else
+  		console.log(1+","+\t");
+	}
+	console.log("\n");
+}
