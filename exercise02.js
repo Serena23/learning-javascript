7cf53c0aff944d4d78ae334ff150bffb9ecd519b
Serena23 <serenasforza@tiscali.it>
2013-03-06 18:37:12 +0100

    exercise03

--- exercise02.js
+++ exercise02.js
@@ -3,9 +3,9 @@ var i;
 for (j = 1; j <= 10; j +=1) {
 	for (i = 1; i <= 10; i +=1) {
   		if(i!==10)
-  			console.log(j*i+","+\t");
+  			console.log(j*i+","+"\t");
   		else
-  		console.log(j*i+\t");
+  		console.log(j*i+"\t");
 	}
 	console.log("\n");
 }
--- exercise03.js
+++ exercise03.js
@@ -4,13 +4,20 @@ for (j = 1; j <= 10; j +=1) {
 	for (i = 1; i <= 10; i +=1) {
   		if(i!==j){
   			if (i!==10) {
-  				console.log(0+","+\t");
-  			else
-  				console.log(0+\t");
+  				console.log(0+","+"\t");
+        }
+  			else{
+  				console.log(0+"\t");
   			}
   		}
-  		else
-  		console.log(1+","+\t");
+  		else{
+        if (i!==10) {
+          console.log(1+","+"\t");
+        }
+        else{
+          console.log(1+"\t");
+        }
+      }
 	}
 	console.log("\n");
 }
